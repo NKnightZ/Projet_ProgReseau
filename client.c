333b572b1d1f38c45f66a0cc470f51a8ea1fe052
Aboubakar Kachaou <aboubakar.kachaou@etu.u-pec.fr>
2021-09-29 19:33:39 +0200

    variable client/server

---
+++ client.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+struct var{
+    double spent;
+    char* name;
+    char* date;
+    char* utility; //for example hotel, market, travel
+};
---
+++ server.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+struct var{
+    double balance;
+    double total;
+    char* utility; 
+};
