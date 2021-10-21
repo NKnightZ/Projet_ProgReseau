8d9554a4e57fbd744ebc8b4b90bc0751dde5a0fa
NKnightZ <aboubakar.du.94@gmail.com>
2021-10-21 13:19:32 +0200

    more information

--- client.c
+++ client.c
@@ -4,5 +4,7 @@ struct var{
     double spent;
     char* name;
     char* date;
-    char* utility; //for example hotel, market, travel
+    char* utility; // for example hotel, market, travel
 };
+
+// faire de l'UDP, (et sinon mettre un compteur de paquet pour verifier qu'il n'y a pas de paquet perdu ça sera un bonus) 
